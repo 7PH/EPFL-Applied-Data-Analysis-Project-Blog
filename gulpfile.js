f1f82ed9befbcbdb78bfd9f5d3c6089bc29b7801
PowerHat <b.raymond@protonmail.com>
2018-12-15 21:26:42 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require("gulp");
+const browserify = require("browserify");
+const source = require('vinyl-source-stream');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
+const tsify = require("tsify");
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const jsObfuscator = require('gulp-javascript-obfuscator');
+const htmlmin = require('gulp-htmlmin');
+const pug = require('gulp-pug');
+
+const pugPaths = { pages: ['app/views/*.pug'] };
+const scssPaths = { pages: ['app/css/*.scss'] };
+const resPaths = { pages: ['app/assets/*'] };
+
+gulp.task("copy-views", () => {
+    return gulp.src(pugPaths.pages)
+        .pipe(pug())
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist/'))
+});
+gulp.task("copy-assets", () => gulp.src(resPaths.pages) .pipe(gulp.dest('dist/assets')));
+
+gulp.task("copy-scss", function () {
+    return gulp.src(scssPaths.pages)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task("typescript", function () {
+    return browserify({
+            basedir: '.',
+            debug: true,
+            entries: ['app/src/main.ts'],
+            cache: {},
+            packageCache: {}
+        })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(jsObfuscator())
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('default', gulp.parallel('copy-views', 'copy-scss', 'copy-assets', 'typescript'));
